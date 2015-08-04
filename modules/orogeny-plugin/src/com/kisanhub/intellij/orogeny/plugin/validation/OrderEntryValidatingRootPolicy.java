2cfbf72a7f58e91cd3b721639e5070763fae6dd9
Raphael Cohn <raphael.cohn@stormmq.com>
2015-08-04 08:16:48 +0100

    Updated copyright

--- modules/orogeny-plugin/src/com/kisanhub/intellij/orogeny/plugin/validation/OrderEntryValidatingRootPolicy.java
+++ modules/orogeny-plugin/src/com/kisanhub/intellij/orogeny/plugin/validation/OrderEntryValidatingRootPolicy.java
@@ -1,6 +1,6 @@
 /*
- * This file is part of intellij-orogeny. It is subject to the licence terms in the COPYRIGHT file found in the top-level directory of this distribution and at https://raw.githubusercontent.com/KisanHub/developjs/master/COPYRIGHT. No part of intellij-orogeny, including this file, may be copied, modified, propagated, or distributed except according to the terms contained in the COPYRIGHT file.
- * Copyright © 2015 The developers of intellij-orogeny. See the COPYRIGHT file in the top-level directory of this distribution and at https://raw.githubusercontent.com/KisanHub/developjs/master/COPYRIGHT.
+ * This file is part of intellij-orogeny. It is subject to the licence terms in the COPYRIGHT file found in the top-level directory of this distribution and at https://raw.githubusercontent.com/KisanHub/intellij-orogeny/master/COPYRIGHT. No part of intellij-orogeny, including this file, may be copied, modified, propagated, or distributed except according to the terms contained in the COPYRIGHT file.
+ * Copyright © 2015 The developers of intellij-orogeny. See the COPYRIGHT file in the top-level directory of this distribution and at https://raw.githubusercontent.com/KisanHub/intellij-orogeny/master/COPYRIGHT.
  */
 
 package com.kisanhub.intellij.orogeny.plugin.validation;
